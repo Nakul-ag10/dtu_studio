3c6b5d285c29110cb8bb1c3c19dd2afa3275c048
Nakul-ag10 <agarwal.nakul10@gmail.com>
2026-04-18 23:18:26 +0530

    Removed test.tsx

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -19,11 +19,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://dtu-studio-1.onrender.com',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://dtu-studio-1.onrender.com',
         changeOrigin: true,
       },
     },
